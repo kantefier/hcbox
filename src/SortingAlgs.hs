b3d2d3bf11f979e6c6dec58636bae9706c8cc039
kantefier <kinebogin@gmail.com>
2016-05-03 23:12:37 +0300

    removed unused import. Changed tabs to spaces, reindented code

--- src/SortingAlgs.hs
+++ src/SortingAlgs.hs
@@ -1,29 +1,27 @@
 module SortingAlgs
 ( bubbleSort ) where
 
-import Data.List (partition)
+  findMax :: (Ord a) => [a] -> a
+  findMax (x:[]) = x
+  findMax (x:y:[]) = max x y
+  findMax (x:xs) = max x (findMax xs)
 
-findMax :: (Ord a) => [a] -> a
-findMax (x:[]) = x
-findMax (x:y:[]) = max x y
-findMax (x:xs) = max x (findMax xs)
+  sorted :: (Ord a) => [a] -> Bool
+  sorted [] = True
+  sorted (_:[]) = True
+  sorted lst @ (x:y:xs) = x <= y && sorted(tail lst)
 
-sorted :: (Ord a) => [a] -> Bool
-sorted [] = True
-sorted (_:[]) = True
-sorted lst @ (x:y:xs) = x <= y && sorted(tail lst)
+  bubbleSort :: (Ord a) => [a] -> [a]
+  bubbleSort [] = []
+  bubbleSort a @ (_:[]) = a
+  bubbleSort lst = until(sorted)(\(x:y:xs) -> (min x y) : bubbleSort((max x y) : xs)) lst
 
-bubbleSort :: (Ord a) => [a] -> [a]
-bubbleSort [] = []
-bubbleSort a @ (_:[]) = a
-bubbleSort lst = until(sorted)(\(x:y:xs) -> (min x y) : bubbleSort((max x y) : xs)) lst
-
-selectSort :: (Ord a) => [a] -> [a]
-selectSort [] = []
-selectSort a @ (_:[]) = a
-selectSort lst =
-	minElem : (selectSort restList)
-	where
-		indexedLst = zip lst [0,1..]
-		(minElem, minElemIdx) = minimum indexedLst
-		(restList, _) = unzip(filter(\(x, idx) -> idx /= minElemIdx) indexedLst)
+  selectSort :: (Ord a) => [a] -> [a]
+  selectSort [] = []
+  selectSort a @ (_:[]) = a
+  selectSort lst =
+    minElem : (selectSort restList)
+    where
+      indexedLst = zip lst [0,1..]
+      (minElem, minElemIdx) = minimum indexedLst
+      (restList, _) = unzip(filter(\(x, idx) -> idx /= minElemIdx) indexedLst)
