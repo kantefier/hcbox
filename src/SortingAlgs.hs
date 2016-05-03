8a76917c905ec0a9d655bca6c18d7cd3010416c1
kantefier <kinebogin@gmail.com>
2016-05-03 23:09:56 +0300

    implemented select sort alg

--- src/SortingAlgs.hs
+++ src/SortingAlgs.hs
@@ -1,6 +1,8 @@
 module SortingAlgs
 ( bubbleSort ) where
 
+import Data.List (partition)
+
 findMax :: (Ord a) => [a] -> a
 findMax (x:[]) = x
 findMax (x:y:[]) = max x y
@@ -8,10 +10,20 @@ findMax (x:xs) = max x (findMax xs)
 
 sorted :: (Ord a) => [a] -> Bool
 sorted [] = True
-sorted (x:[]) = True
+sorted (_:[]) = True
 sorted lst @ (x:y:xs) = x <= y && sorted(tail lst)
 
 bubbleSort :: (Ord a) => [a] -> [a]
 bubbleSort [] = []
-bubbleSort (x:[]) = x:[]
+bubbleSort a @ (_:[]) = a
 bubbleSort lst = until(sorted)(\(x:y:xs) -> (min x y) : bubbleSort((max x y) : xs)) lst
+
+selectSort :: (Ord a) => [a] -> [a]
+selectSort [] = []
+selectSort a @ (_:[]) = a
+selectSort lst =
+	minElem : (selectSort restList)
+	where
+		indexedLst = zip lst [0,1..]
+		(minElem, minElemIdx) = minimum indexedLst
+		(restList, _) = unzip(filter(\(x, idx) -> idx /= minElemIdx) indexedLst)
